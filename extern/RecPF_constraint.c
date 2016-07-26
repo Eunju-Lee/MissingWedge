4e366a08fde81b0807d963391a0b2246e218a91c
Eunju <slzkdskzkd@naver.com>
2016-07-26 11:23:37 +0900

    upload
    201607261120

--- extern/RecPF_constraint.c
+++ extern/RecPF_constraint.c
@@ -5,7 +5,7 @@
 #include "enhance.h"
 
 void RecPF_constraint(int m,int n,double aTV, double aL1,CvMat* picks,CvMat* B,
-                      int a,/*struct OPTS* st*/double aa ,CvMat* WT,CvMat* W,int range,IplImage* I,int constraint, IplImage* U)
+                      int TVtype,/*struct OPTS* st*/double aa ,CvMat* WT,CvMat* W,int range,IplImage* I,int constraint, IplImage* U)
 {
 
     int bPrint = 0;
@@ -38,7 +38,7 @@ void RecPF_constraint(int m,int n,double aTV, double aL1,CvMat* picks,CvMat* B,
         aL1 = count/sqrt(m*n)*aL1;
     }
 
-    CvMat *Numer1, *Denom1;
+    CvMat *Numer1, *Denom1, *Denom2, *Denom;
     cvZero(Numer1);
 
     int j, i;
@@ -53,4 +53,90 @@ void RecPF_constraint(int m,int n,double aTV, double aL1,CvMat* picks,CvMat* B,
     //denom2
 
 
+    if(aL1 == 0){
+        cvmAdd(Denom1, Denom2, Denom);
+    }
+    else{
+        cvmAdd(Denom1, Denom2, Denom);
+        for(j = 1; j<Denom->cols; j++){
+            for(i = 1; i<Denom->rows; i++){
+                double element = cvmGet(Denom, i, j) + aL1*/*beta*/2.0;
+                cvmSet(Denom,i, j, element);
+            }
+        }
+    }
+
+    CvMat* Ux, *Uy, *bx, *by, *d, *d_penalty, *PsiTU;
+    cvSetZero(Ux);
+    cvSetZero(Uy);
+    cvSetZero(bx);
+    cvSetZero(by);
+    cvSetZero(d);
+    cvSetZero(d_penalty);
+
+    if(aL1 > 0){
+        cvSetZero(PsiTU);
+        cvSetZero(d);
+    }
+
+
+    double MaxItr;
+    double beta = 2.0;
+    int ii;
+    CvMat* Wx,Wy;
+
+        for(ii =0; ii<MaxItr; ii++){
+
+            switch(TVtype){
+                case 1 :
+                    cvmAdd(Ux, bx, Ux);
+                    cvmAdd(Uy, by, Uy);
+                break;
+
+                case 2 :
+                    Compute_Wx_Wy(Ux, Uy, bx, by, 1/beta, &Wx, &Wy);
+                break;
+                default :
+                    printf("TVtype must be 1 or 2\n");
+                break;
+
+            }
+            if(aL1 > 0){
+
+                cvmAdd(PsiTU,d,PsiTU);
+                int i,j;
+                for(j = 1; j<PsiTU->cols; j++){
+                    for(i = 1; i<PsiTU->rows; i++){
+
+                        double element_sin = sin(cvmGet(PsiTU, i, j));
+                        double element_abs = abs(cvmGet(PsiTU, i, j));
+
+                        if(element_abs-1/beta > 0){
+                            cvmSet(PsiTU,i,j,element_abs*element_sin);
+                        }
+                        else{
+                            cvmSet(PsiTU,i,j,0.);
+                        }
+
+                    }
+                }
+
+            }
+
+
+
+    }//for
+
+
+    cvReleaseMat(&PsiTU);
+    cvReleaseMat(&Numer1);
+    cvReleaseMat(&Denom1);
+    cvReleaseMat(&Denom2);
+    cvReleaseMat(&Denom);
+    cvReleaseMat(&Ux);
+    cvReleaseMat(&Uy);
+    cvReleaseMat(&bx);
+    cvReleaseMat(&by);
+    cvReleaseMat(&d);
+    cvReleaseMat(&d_penalty);
 }
